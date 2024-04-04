b97367e6a688659269f49c4b7d3019d7a616fae3
eike9000 <eike9000@web.de>
2024-04-04 23:29:45 +0200

    button label change in maxtimescreen

--- maxtimescreen.py
+++ maxtimescreen.py
@@ -18,8 +18,9 @@ class MaxTimeScreen(Screen):
         self.layout.add_widget(Label(text="Maximalversuch", size_hint=(0.5, 0.1), pos_hint={'center_x': 0.5, 'y': 0.9}))
         self.timer_label = Label(text="00:00", font_size="30pt",  size_hint=(0.9, 0.4), pos_hint={'x': 0.05, 'y': 0.6})
         self.layout.add_widget(self.timer_label)
-        self.layout.add_widget(Button(text="Start/Stop", size_hint=(0.9, 0.5), pos_hint={'x': 0.05, 'y': 0.1},
-                                      on_press=self.on_actionbutton_press))
+        self.action_button = Button(text="Start", size_hint=(0.9, 0.5), pos_hint={'x': 0.05, 'y': 0.1},
+                                    on_press=self.on_actionbutton_press)
+        self.layout.add_widget(self.action_button)
         self.layout.add_widget(Button(text="back", size_hint=(0.1, 0.1), pos_hint={'x': 0.05, 'y': 0.9},
                                       on_press=self.on_backbutton_press))
         self.layout.bind(size=self.update_rect)
@@ -27,21 +28,23 @@ class MaxTimeScreen(Screen):
     def on_actionbutton_press(self, _instance):
         if self.clock_is_running:
             self.clock.cancel()
+            self.action_button.text = "Start"
         else:
             self.elapsed_time = 50
             self.clock = Clock.schedule_interval(self.clock_callback, 0.1)
+            self.action_button.text = "Stop"
         self.clock_is_running = not self.clock_is_running
 
     def clock_callback(self, tick):
         self.elapsed_time += tick
-        minutes = math.trunc(self.elapsed_time // 60)
-        seconds = math.trunc(self.elapsed_time % 60)
+        time = math.trunc(self.elapsed_time)
+        minutes = time // 60
+        seconds = time % 60
         if minutes > 0:
             time_text = "{0}:{1:02d} min".format(minutes, seconds)
         else:
             time_text = "{0:d} s".format(seconds)
         self.timer_label.text = time_text
-        print(tick, self.elapsed_time, time_text)
 
     def on_backbutton_press(self, _instance):
         self.manager.current = "MenuScreen"
