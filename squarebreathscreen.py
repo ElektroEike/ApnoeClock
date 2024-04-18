31cda1b18b4eca26830a18b08a79cebbee491115
Eike Lange <eike9000@web.de>
2024-04-18 23:09:44 +0200

    show user what to do in squarebreath exercise

--- squarebreathscreen.py
+++ squarebreathscreen.py
@@ -12,19 +12,18 @@ class SquareBreathScreen(Screen):
         self.parent_screen_name = parentname
         # states: StateNum: (LabelText, DurationOfState, NextStateNum), ...
         # NextStateNum==-1 ==> end
-        self.states = {0: ("Prepare...", 5, 1),
+        self.states = {0: ("Prepare...", 3, 1),
                        1: ("Inhale", 4, 2),
-                       2: ("Hold Breath", 3, 3),
-                       3: ("Exhalte", 5, 4),
-                       4: ("Hold Breath", 4, 1)}
+                       2: ("Hold Breath", 5, 3),
+                       3: ("Exhale", 6, 4),
+                       4: ("Hold Breath", 7, 1)}
 
         self.layout = FloatLayout(size_hint=(1, 1))
         self.add_widget(self.layout)
-
         self.layout.add_widget(Label(text="Square Breath", size_hint=(0.5, 0.1), pos_hint={'center_x': 0.5, 'y': 0.9}))
-        self.label_todo = Label(text="ToDo", font_size="30pt",  size_hint=(0.9, 0.4), pos_hint={'x': 0.05, 'y': 0.56})
+        self.label_todo = Label(text="ToDo", font_size="30pt", size_hint=(0.9, 0.1), pos_hint={'x': 0.05, 'y': 0.8})
         self.layout.add_widget(self.label_todo)
-        self.label_time = Label(text="00:00", font_size="30pt", size_hint=(0.9, 0.4), pos_hint={'x': 0.05, 'y': 0.48})
+        self.label_time = Label(text="00:00", font_size="30pt", size_hint=(0.9, 0.1), pos_hint={'x': 0.05, 'y': 0.72})
         self.layout.add_widget(self.label_time)
         self.action_button = Button(text="Start/Stop", size_hint=(0.98, 0.5), pos_hint={'x': 0.01, 'y': 0.01},
                                     on_press=self.on_startstop_press)
@@ -32,16 +31,47 @@ class SquareBreathScreen(Screen):
         self.layout.add_widget(Button(text='↩', font_name='DejaVuSans', font_size="20pt",
                                       size_hint=(0.2, 0.1), pos_hint={'x': 0.01, 'y': 0.89},
                                       on_press=self.on_backbutton_press))
+        # show, what the user has to do in the states
+        self.label_todo_states = []
+        text = f'{self.states[1][0]} {self.states[1][1]} s'
+        label_todo_state1 = Label(text=text, font_size="14pt",
+                                  size_hint=(0.48, 0.05), pos_hint={'x': 0.01, 'y': 0.64})
+        self.layout.add_widget(label_todo_state1)
+        self.label_todo_states.append(label_todo_state1)
 
+        text = f'{self.states[2][0]} {self.states[2][1]} s'
+        label_todo_state2 = Label(text=text, font_size="14pt",
+                                  size_hint=(0.48, 0.05), pos_hint={'x': 0.5, 'y': 0.64})
+        self.layout.add_widget(label_todo_state2)
+        self.label_todo_states.append(label_todo_state2)
+
+        text = f'{self.states[3][0]} {self.states[3][1]} s'
+        label_todo_state3 = Label(text=text, font_size="14pt",
+                                  size_hint=(0.48, 0.05), pos_hint={'x': 0.5, 'y': 0.55})
+        self.layout.add_widget(label_todo_state3)
+        self.label_todo_states.append(label_todo_state3)
+
+        text = f'{self.states[4][0]} {self.states[4][1]} s'
+        label_todo_state4 = Label(text=text, font_size="14pt",
+                                  size_hint=(0.48, 0.05), pos_hint={'x': 0.01, 'y': 0.55})
+        self.layout.add_widget(label_todo_state4)
+        self.label_todo_states.append(label_todo_state4)
+
+        # draw lines arround border as a progress bar duing exercise
         self.colors = []
-        self.ellipses = []
         self.lines = []
         self.points = []        # filled and updated in update_rect()
         with self.canvas:
             for i in range(0, 4):
                 self.colors.append(Color(0, 0, 1, .5, mode='rgba'))
-                self.ellipses.append(Ellipse(size=(0, 0), size_hint=(None, None)))
-                self.lines.append(Line(width=3))
+                self.lines.append(Line(width=2))
+
+        # draw a rect arround label_todo_next
+        self.labelrects = []
+        with self.label_todo_states[0].canvas.before:  # seem to work for all labels. don't know why but it's ok.
+            for i in range(0, 4):
+                Color(0.6, 0.6, 0.6)
+                self.labelrects.append(Rectangle())
 
         self.current_state_num = -2     # start after preparation
 
@@ -53,9 +83,11 @@ class SquareBreathScreen(Screen):
         self._prepare()
 
     def on_leave(self, *args):
+        """ if you leave and enter again, it looks ugly if colors jump arround. so we reset the colors here """
         self.clock.reset()
         self._reset_color()
         self._reset_lines()
+        self._reset_labels()
 
     def stateclock_reports(self, reason, label, duration, time):
         self.label_todo.text = label
@@ -75,10 +107,10 @@ class SquareBreathScreen(Screen):
             pass
 
     def _reset_color(self):
-        self.colors[0].rgba = (0, 0, 1, 0.5)
-        self.colors[1].rgba = (0, 0, 1, 0.5)
-        self.colors[2].rgba = (0, 0, 1, 0.5)
-        self.colors[3].rgba = (0, 0, 1, 0.5)
+        self.colors[0].rgb = (0.5, 0.5, 0.6)
+        self.colors[1].rgb = (0.5, 0.5, 0.6)
+        self.colors[2].rgb = (0.5, 0.5, 0.6)
+        self.colors[3].rgb = (0.5, 0.5, 0.6)
 
     def _reset_lines(self):
         self.lines[0].points = self.points[0:4]
@@ -86,18 +118,31 @@ class SquareBreathScreen(Screen):
         self.lines[2].points = self.points[4:8]
         self.lines[3].points = self.points[6:10]
 
+    def _reset_labels(self):
+        for i in range(0, 4):
+            self.label_todo_states[i].bold = False
+            self.label_todo_states[i].color = (1, 1, 1)
+
+
     def _prepare(self):
         """ for next start """
         self.current_state_num = -2
         self._reset_lines()
         self._reset_color()
+        self._reset_labels()
         self.action_button.text = "Start"
         self.clock.init()
 
     def _set_color_from_state(self, statenum):
+        """ depending on the state, we set the color of each line
+            note: this is not the real state num, as "prepare state"
+                does not count """
         self._reset_color()
+        self._reset_labels()
         for i in range(statenum + 1):
             self.colors[i].rgba = (0, 1, 0, 1)
+        self.label_todo_states[statenum].bold = True
+        self.label_todo_states[statenum].color = (0, 1, 0, 0.5)
 
     def _set_lines_from_state(self, statenum, percent):
         """ this shows a moving line. Line length is calculated by the
@@ -128,22 +173,17 @@ class SquareBreathScreen(Screen):
             Color(0.5, 0.5, 0.5)
             Rectangle(pos=self.layout.pos, size=self.layout.size)
 
+        # draw lines along the border
         width, height = self.size
-        min_size = min(width, height)
-        eli_size = min_size / 20
-        x0 = width / 10
-        x1 = 9 * width / 10 - eli_size
-        y0 = 8.3 * height / 10
-        y1 = 5.3 * height / 10
-        # position of circles
-        self.ellipses[0].size = (eli_size, eli_size)
-        self.ellipses[0].pos = (x0, y0)
-        self.ellipses[1].size = (eli_size, eli_size)
-        self.ellipses[1].pos = (x1, y0)
-        self.ellipses[2].size = (eli_size, eli_size)
-        self.ellipses[2].pos = (x1, y1)
-        self.ellipses[3].size = (eli_size, eli_size)
-        self.ellipses[3].pos = (x0, y1)
+        w2 = width - 1
+        h2 = height - 1
         # draw rect connecting all points
-        self.points = [x0 + eli_size, y0, x1, y0, x1, y1 + eli_size, x0 + eli_size, y1 + eli_size, x0 + eli_size, y0]
+        self.points = [1, h2, w2, h2, w2, 0, 1, 0, 1, h2]
         self._reset_lines()
+
+        for i in range(0, 4):
+            x = self.label_todo_states[i].x
+            y = self.label_todo_states[i].y
+            size = self.label_todo_states[i].size
+            self.labelrects[i].pos =(x, y)
+            self.labelrects[i].size=size
